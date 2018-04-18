24c897d2ebb2968d7fc77f8d81769afd24563a6e
Tiller <ctiller@proforma.com>
2018-04-18 14:26:22 -0400

    Create movie model

---
+++ Movie_API/DbContext/MoviesContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Movie_API.Data
+{
+    public class MoviesContext:DbContext
+    {
+        public MoviesContext():base("name=DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ Movie_API/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie_API.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Rating { get; set; }
+    }
+}
