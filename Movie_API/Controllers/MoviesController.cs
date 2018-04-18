765b52f192b0d738ab82e4d2d4a4337fa2ddbb78
Tiller <ctiller@proforma.com>
2018-04-18 14:15:59 -0400

    Create base movie controller

---
+++ Movie_API/Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Movie_API.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        public IEnumerable<String> Get()
+        {
+            return new List<string> { "Die Hard", "Speed Racer", "The Nightmare before christmas", "Bee Movie" };
+        }
+    }
+}
